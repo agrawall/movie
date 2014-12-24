10a7ed1ec4c55f0b77de7c55fd29159180cb8d92
Agrawal <agrawall@685b359c0181.ant.amazon.com>
2014-12-24 21:52:04 +0530

    added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+  * A Person Class.
+  */
+
+class Person{
+      private String name;
+      private String age;
+ 
+      public getName()
+      {
+          return name;
+      }
+ 
+      public setName(String name)
+      {
+         this.name = name;
+      }
+      
+}
